698a6472e4095ce7e4f7bc01b79e79f8a3d27d08
Andrei Cosma <andrei.cosma55@gmail.com>
2023-11-09 06:54:29 +0200

    comment cards wrapper

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -14,20 +14,18 @@ const iconMap = {
 };
 
 export default async function CardWrapper() {
-   const totalPaidInvoices = [];
-   const totalPendingInvoices = [];
-   const numberOfInvoices = 0;
-   const numberOfCustomers = 0;
   return (
     <>
-      <Card title="Collected" value={totalPaidInvoices} type="collected" />
-      <Card title="Pending" value={totalPendingInvoices} type="pending" />
-      <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
-      <Card
-        title="Total Customers"
-        value={numberOfCustomers}
-        type="customers"
-      />
+      {/* NOTE: comment in this code when you get to this point in the course */}
+
+           {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
+           <Card title="Pending" value={totalPendingInvoices} type="pending" />
+           <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
+           <Card
+             title="Total Customers"
+             value={numberOfCustomers}
+             type="customers"
+           /> */}
     </>
   );
 }
