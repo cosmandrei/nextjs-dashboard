d29b5f4990b22ee6273c8e3f3f378e9150ab8a09
Andrei Cosma <andrei.cosma55@gmail.com>
2023-11-10 08:45:10 +0200

    fix missplaced suspense attribute

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -26,8 +26,8 @@ export default async function Page() {
                 <Suspense fallback={<RevenueChartSkeleton/>}>
                     <RevenueChart />
                 </Suspense>
-                <Suspense>
-                    <LatestInvoices fallback={<LatestInvoicesSkeleton/>}/>
+                <Suspense fallback={<LatestInvoicesSkeleton/>}>
+                    <LatestInvoices />
                 </Suspense>
             </div>
         </main>
